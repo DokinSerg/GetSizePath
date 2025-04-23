c792d0d02b42c591f3e3db6bc307590349119d19
Sergey Dokin <dokin.serg@gmail.com>
2025-04-23 20:25:41 +0300

    Update GetSizePath.py

--- GetSizePath.py
+++ GetSizePath.py
@@ -1,5 +1,5 @@
 import os
-import sys
+# import sys
 # import uuid
 import traceback
 import asyncio
@@ -11,12 +11,13 @@ from time import perf_counter
 from rich import print as rpn
 #------------------------------------------
 __author__ = 't.me/dokin_sergey'
-__version__ = '0.0.4'
-__verdate__ = '2025-04-18 12:04'
-SourcePath = 'C:\\Users'
+__version__ = '0.0.5'
+__verdate__ = '2025-04-23 19:57'
+# SourcePath = r'c:\Users\dokin\AppData\Local\Yandex\YandexBrowser\User Data\Default'
 semaphore = asyncio.Semaphore(7000)
 WaitPrBar = True#ProgressBar Stop
 kGb = 1048576
+
 ###############################################################################################################
 async def ProgressBar()-> None:
     i=0
@@ -77,66 +78,81 @@ async def ParsingPath_A(CurPath:str)->tuple[str, int, int]:
         rpn(f'[red1]PP:{traceback.format_exc()}')
     return CurPath,cursize,counfile
 ##############################################################################################################
-async def main()-> None:
+async def main(SourcePath:str)-> None:
     global WaitPrBar
-    DirSize:dict[str,int] = {}
-    all_size = 0
-    CountFiles = 0
-    try:
-        tasks = []
-        rpn(f'Расчет объёма диреторий для {SourcePath}')
+    #-----------------------------------------------------------------------------
+    while True:
+        DirSize:dict[str,int] = {}
+        all_size = 0
+        CountFiles = 0
         start_Src = perf_counter()
-        WaitPrBar = True
-        tsc = asyncio.create_task(ProgressBar(),name='ProgressBar')
-        with await aos.scandir(SourcePath) as itPaths:
-            for item in itPaths:
-                await asyncio.sleep(0.01)
-                if item.is_dir(follow_symlinks=False):
-                    task = asyncio.create_task(ParsingPath_A(item.path))
-                    await asyncio.sleep(0)
-                    tasks.append(task)
-                    continue
-                if item.is_file(follow_symlinks=False):
-                    CountFiles += 1
-                    all_size += item.stat().st_size
-    #--------------------------------------------------------------------------------------
-        await asyncio.gather(*tasks)
-        await asyncio.sleep(0.01)
-        # #--------------------------------------------------------------------------------
-        for task in tasks:
-            CountFiles += task.result()[2]
-            all_size += task.result()[1]
-            DirSize[task.result()[0]]  = task.result()[1]
-    #--------------------------------------------------------------------------------------
-        WaitPrBar = False
-        await tsc
-        if not tsc.done():tsc.get_coro().close()
-        await asyncio.sleep(0.01)
-    #--------------------------------------------------------------------------------------
+        try:
+            tasks = []
+            rpn(f'Расчет объёма диреторий для {SourcePath}')
+            WaitPrBar = True
+            tsc = asyncio.create_task(ProgressBar(),name='ProgressBar')
+            with await aos.scandir(SourcePath) as itPaths:
+                for item in itPaths:
+                    await asyncio.sleep(0.01)
+                    if item.is_dir(follow_symlinks=False):
+                        task = asyncio.create_task(ParsingPath_A(item.path))
+                        await asyncio.sleep(0)
+                        tasks.append(task)
+                        continue
+                    if item.is_file(follow_symlinks=False):
+                        CountFiles += 1
+                        all_size += item.stat().st_size
+        #--------------------------------------------------------------------------------------
+            await asyncio.gather(*tasks)
+            await asyncio.sleep(0.01)
+            # #--------------------------------------------------------------------------------
+            for task in tasks:
+                CountFiles += task.result()[2]
+                all_size += task.result()[1]
+                DirSize[task.result()[0]]  = task.result()[1]
+        #--------------------------------------------------------------------------------------
+            WaitPrBar = False
+            await tsc
+            if not tsc.done():
+                tsc.get_coro().close()
+            await asyncio.sleep(0.01)
+        #--------------------------------------------------------------------------------------
+        except Exception as err:
+            rpn(f'[red1]Main:{err}')
+            rpn(f'[red1]Main:{traceback.format_exc()}')
+        #------------------------------------------------------------------------------------------
         sizekb = round(all_size/kGb,3)
         rpn()
+        rpn(f'[green1]{SourcePath}\n')
         sortDirSize = dict(sorted(DirSize.items(),key=lambda itm: itm[1],reverse=True))
-        for i,j in sortDirSize.items():
-            fsn = f'{i[:40]}[khaki1]~' if len(i) > 40 else i
-            rpn(f'[cyan1]{fsn:41} [green1]{j/kGb:10_.3f}')
+        Strlen = 30
+        for i,j in enumerate(sortDirSize.items()):
+            ipath = os.path.basename(j[0])
+            fsn = f'{ipath[:Strlen]} [khaki1]~' if len(ipath) > Strlen else ipath
+            rpn(f'[green1]{i:3} [cyan1]{fsn:32} [green1]{j[1]/kGb:12_.3f}')
         rpn(f'\n[cyan1]Всего файлов [khaki1]{CountFiles:_} [cyan1]общим размером [khaki1]{all_size:_} [cyan1]байт ([khaki1]{sizekb:_} Mb)\n')
         stop_Src = perf_counter()
         FileListTime = round(stop_Src - start_Src,3)
         rpn(f'Составление списка файлов заняло {FileListTime}c')
-    #--------------------------------------------------------------------------------------
-
-        #-----------------------------------------------------------------------------
-        # await asyncio.create_task(LogMessage(f'Копирование завершено. Скопировано {len(FileList)} файлов. Время копирования {copy_time} c'))
-        # await asyncio.sleep(0.1)
+        #--------------------------------------------------------------------------------------
+        while True:
+            if not (kye := input('Номер папки? ')):return
+            if kye.isdigit() and int(kye) < len(sortDirSize):
+                curDir = list(enumerate(sortDirSize))[int(kye)][1]
+                SourcePath = os.path.join(SourcePath,curDir)
+                rpn(SourcePath)
+                break
+        rpn('#'*100)
     #-------------------------------------------------------------------------------------
-    except Exception as err:
-        rpn(f'[red1]Main:{err}')
-        rpn(f'[red1]Main:{traceback.format_exc()}')
 ###############################################################################################################
 start_time = perf_counter()
-asyncio.run(main())
-stop_time = perf_counter()
-Inetrval = stop_time - start_time
-print(f'\nОбщее время выполнения {Inetrval:.7f}')
-if not (len(sys.argv) > 1 and sys.argv[1] == 'cons'):input(':-> ')
+StartPath = 'C:\\'
+if not (key := input(f'Введите букву диска [{StartPath}] :-)>')):
+    key = StartPath
+if key =='0':os._exit(0)
+dr,_ = os.path.splitdrive(key)
+if not dr: key = f'{key}:\\'
+elif dr == StartPath:key = f'{key}\\'
+asyncio.run(main(key))
+input(':-> ')
 os._exit(0)
